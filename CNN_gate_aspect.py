6fbe70461524a7e9a293f80a4bd0060722ab9148
SanyuR132 <sanyu.rajakumar@gmail.com>
2022-05-11 03:45:09 -0400

    fix model

--- CNN_gate_aspect.py
+++ CNN_gate_aspect.py
@@ -1,25 +1,33 @@
 import tensorflow as tf
 from tensorflow import keras
 
-class CNN_Gate_Aspect_Text(tf.keras.model):
+
+class CNN_Gate_Aspect_Text(tf.keras.Model):
     def __init__(self, feature_embeddings, aspect_embeddings):
-        D = feature_embeddings.shape[1] ## embedding dimension
-        C = 4 ## no. of output classes
-        Co = 3 ## no. of kernels/filters
+        super(CNN_Gate_Aspect_Text, self).__init__()
+        D = feature_embeddings.shape[1]  # embedding dimension
+        C = 4  # no. of output classes
+        Co = 3  # no. of kernels/filters
 
-        self.feature_embedding_layer = tf.keras.layers.Embedding(len(feature_embeddings), D, 
-        embeddings_initializer= keras.initializers.Constant(feature_embeddings), trainable = True)
+        self.feature_embedding_layer = tf.keras.layers.Embedding(len(feature_embeddings), D,
+                                                                 embeddings_initializer=keras.initializers.Constant(feature_embeddings), trainable=True)
 
-        self.aspect_embedding_layer = tf.keras.layers.Embedding(len(aspect_embeddings), D, 
-        embeddings_initializer= keras.initializers.Constant(aspect_embeddings), trainable = True)
+        self.aspect_embedding_layer = tf.keras.layers.Embedding(len(aspect_embeddings), D,
+                                                                embeddings_initializer=keras.initializers.Constant(aspect_embeddings), trainable=True)
 
-        self.conv_layer_11 = tf.nn.conv1d(D, Co, 3)
-        self.conv_layer_12 = tf.nn.conv1d(D, Co, 4)
-        self.conv_layer_13 = tf.nn.conv1d(D, Co, 5)
+        self.conv_layer_11 = tf.keras.layers.Conv1D(
+            Co, 3, input_shape=(None, D))
+        self.conv_layer_12 = tf.keras.layers.Conv1D(
+            Co, 4, input_shape=(None, D))
+        self.conv_layer_13 = tf.keras.layers.Conv1D(
+            Co, 5, input_shape=(None, D))
 
-        self.conv_layer_21 = tf.nn.conv1d(D, Co, 3)
-        self.conv_layer_22 = tf.nn.conv1d(D, Co, 4)
-        self.conv_layer_23 = tf.nn.conv1d(D, Co, 5)
+        self.conv_layer_21 = tf.keras.layers.Conv1D(
+            Co, 3, input_shape=(None, D))
+        self.conv_layer_22 = tf.keras.layers.Conv1D(
+            Co, 4, input_shape=(None, D))
+        self.conv_layer_23 = tf.keras.layers.Conv1D(
+            Co, 5, input_shape=(None, D))
 
         self.fully_connected = tf.keras.layers.Dense(C)
         self.fc_aspect = tf.keras.layers.Dense(Co)
@@ -28,19 +36,28 @@ class CNN_Gate_Aspect_Text(tf.keras.model):
 
         feature = self.feature_embedding_layer(feature)
         aspect_v = self.aspect_embedding_layer(aspect)
-        aspect_v = aspect_v.sum(1) / aspect_v.size(1)
+        aspect_v = tf.math.reduce_sum(
+            aspect_v, 1) / tf.cast(tf.shape(aspect_v)[1], dtype=tf.float32)
 
-        x = [tf.nn.tanh(self.conv_layer_11(feature.transpose(1,2))),
-            tf.nn.tanh(self.conv_layer_12(feature.transpose(1,2))),
-            tf.nn.tanh(self.conv_layer_13(feature.transpose(1,2)))]
+        x = [tf.nn.tanh(self.conv_layer_11(feature)),
+             tf.nn.tanh(self.conv_layer_12(feature)),
+             tf.nn.tanh(self.conv_layer_13(feature))]
 
-        y =  [tf.nn.relu(self.conv_layer_21(feature.transpose(1,2)) + self.fc_aspect(aspect_v)),
-              tf.nn.relu(self.conv_layer_22(feature.transpose(1,2)) + self.fc_aspect(aspect_v)),
-              tf.nn.relu(self.conv_layer_23(feature.transpose(1,2)) + self.fc_aspect(aspect_v))]
+        y = [tf.nn.relu(self.conv_layer_21(feature) + self.fc_aspect(tf.expand_dims(aspect_v, 0))),
+             tf.nn.relu(self.conv_layer_22(feature) +
+                        self.fc_aspect(tf.expand_dims(aspect_v, 0))),
+             tf.nn.relu(self.conv_layer_23(feature) + self.fc_aspect(tf.expand_dims(aspect_v, 0)))]
 
-        x = [i*j for i,j in zip(x,y)]
+        x = [i * j for i, j in zip(x, y)]
 
-        x0 = [tf.keras.layers.MaxPooling1D(i, i.size(2)).squeeze(2) for i in x]
+        x0 = [tf.keras.layers.MaxPool1D(
+            pool_size=int(tf.shape(i)[2]))(i) for i in x]
+
+        x0 = [tf.reshape(i, (tf.shape(i)[0], -1)) for i in x]
+
         x0 = tf.concat(x0, 1)
         logit = self.fully_connected(x0)
+
+        print(tf.shape(logit))
+
         return logit, x, y
